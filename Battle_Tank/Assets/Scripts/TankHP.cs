0ea408591a69d6924df8c71b6826b06f08724e74
Syou1215 <syou083121@icloud.com>
2026-05-08 17:36:10 +0900

    タンク_hp表示

--- Battle_Tank/Assets/Scripts/TankHP.cs
+++ Battle_Tank/Assets/Scripts/TankHP.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class TankHP : MonoBehaviour
 {
     public int HP;
+
+    public TextMeshProUGUI HPLabel;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("EnemyShell"))
         {
             HP -= 1;
+
+            HPLabel.text = "" + HP;
 
             if (HP < 1)
             {
