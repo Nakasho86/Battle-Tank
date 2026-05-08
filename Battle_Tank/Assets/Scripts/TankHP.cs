2477ef06806a86caa45d6b029e8dd17ec18374d7
Syou1215 <syou083121@icloud.com>
2026-05-08 19:09:02 +0900

    回復アイテム作成

--- Battle_Tank/Assets/Scripts/TankHP.cs
+++ Battle_Tank/Assets/Scripts/TankHP.cs
@@ -7,6 +7,15 @@ public class TankHP : MonoBehaviour
 
     public TextMeshProUGUI HPLabel;
 
+    public int maxHP = 8; //最大HP
+    public AudioClip itemSound;
+    public GameObject effectPrefab;
+
+    void Start()
+    {
+        HPLabel.text = "" + HP; 
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("EnemyShell"))
@@ -18,6 +27,32 @@ public class TankHP : MonoBehaviour
             if (HP < 1)
             {
                 this.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HPItem"))
+        {
+            //HPアイテムを消す
+            Destroy(other.gameObject);
+
+            //効果音を出す
+            AudioSource.PlayClipAtPoint(itemSound,transform.position);
+
+            //エフェクトを消す
+            Destroy(effectPrefab, 1.0f);
+
+            //HPを「２」回復させる
+            HP += 2;
+            HPLabel.text = "" + HP;
+
+            //HPが最大値を超えないように制限する
+            if (HP > maxHP)
+            {
+                HP = maxHP;
+                HPLabel.text = "" + HP;
             }
         }
     }
